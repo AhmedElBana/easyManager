0a36914a1d4aa3efcf73adb022b2380ef7334bdf
Ahmed Elbanna <ahmedelbanna@weaccept.co>
2019-11-02 21:13:58 +0200

    add customer model

---
+++ db/models/customer.js
@@ -0,0 +1,119 @@
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate-v2');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
+const bcrypt = require('bcryptjs');
+
+let CustomerSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+		minlenght: 2,
+		trim: true
+	},
+	password: {
+		type: String,
+		minlength: 6,
+		trim: true
+	},
+	phoneNumber: {
+		type: String,
+		trim: true,
+		unique: true,
+		required: true
+	},
+	parent: {
+		type: String,
+		trim: true
+	},
+	code: {
+		type: String,
+		trim: true
+	},
+	register_completed: {
+		type: Boolean,
+		required: true
+	},
+	is_login: {
+		type: Boolean,
+		required: true
+	},
+});
+
+CustomerSchema.methods.toJSON = function(){
+	let Customer = this;
+	let CustomerObject = Customer.toObject();
+	return _.pick(CustomerObject, ['_id','name','phoneNumber','parent','register_completed']);
+}
+
+CustomerSchema.methods.generateAuthToken = function(){
+	let customer = this;
+	let access = 'customerToken';
+	let token = jwt.sign({_id: (customer._id), access}, process.env.JWT_SECRET).toString();
+	return token
+}
+CustomerSchema.statics.findByToken = function(token){
+	let Customer = this;
+	let decoded;
+	try {
+	  decoded = jwt.verify(token, process.env.JWT_SECRET);
+	} catch(err) {
+		return Promise.reject();
+	}
+	if(decoded.access === 'customerToken'){
+		return Customer.findOne({
+			'_id': decoded._id
+		});
+	}else{
+		return Promise.reject();
+	}
+}
+CustomerSchema.statics.findByCredentials = function(phoneNumber, password){
+	Customer = this;
+	return Customer.findOne({phoneNumber}).then((customer) => {
+		if(!customer){
+			return Promise.reject();
+		}
+		return new Promise((resolve, reject) => {
+			bcrypt.compare(password, customer.password, (err, res) => {
+				if(res){
+					resolve(customer);
+				}else{
+					reject();
+				}
+			});
+		});
+	});
+}
+CustomerSchema.pre('save', function(next){
+	let customer = this;
+	if(customer.isModified('password')){
+		bcrypt.genSalt(10, (err, salt) => {
+			bcrypt.hash(customer.password, salt, (err, hash) => {
+				customer.password = hash;
+				next();
+			});
+		});
+	}else{
+		next();
+	}
+});
+CustomerSchema.pre('findOneAndUpdate', function(next){
+	let customer = this;
+	if(customer._update['password']){
+		bcrypt.genSalt(10, (err, salt) => {
+			bcrypt.hash(customer._update['password'], salt, (err, hash) => {
+				customer._update['password'] = hash;
+				next();
+			});
+		});
+	}else{
+		next();
+	}
+});
+
+CustomerSchema.plugin(mongoosePaginate);
+let Customer = mongoose.model('Customer', CustomerSchema);
+
+module.exports = {Customer}
