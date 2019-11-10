55d595bf86d55bad644a364a9f41012df86b6624
Ahmed Elbanna <ahmedelbanna@weaccept.co>
2019-11-10 18:17:37 +0200

    complete orders list

--- routes/order.js
+++ routes/order.js
@@ -450,46 +450,86 @@ router.post('/cancel', authenticate, function(req, res, next) {
     }
 });
 
-/* list feature. */
+/* list orders. */
 router.get('/list', authenticate, function(req, res, next) {
-    let page;
-    if(req.query.page){page = req.query.page;}else{page = 1;}
-    let page_size;
-    if(req.query.page_size){page_size = req.query.page_size;}else{page_size = 10;}
-    const options = {
-        page: page,
-        limit: page_size,
-        sort: { createdAt: -1 },
-        collation: {
-        locale: 'en'
-        }
-    };
-    let filters;
-    if(req.user.type == 'admin'){
-        filters = {parent: req.user._id};
-    }else if(req.user.type == 'staff'){
-        filters = {parent: req.user.parent};
-    }
-    Feature.paginate(filters, options, function(err, result) {
-        let next;
-        if(result.hasNextPage){
-            next = "https://" + req.headers.host + "/api/category/feature?page=" + result.nextPage + "&page_size=" + page_size;
-        }else{next = null;}
-        let prev;
-        if(result.hasPrevPage){
-            prev = "https://" + req.headers.host + "/api/category/feature?page=" + result.prevPage + "&page_size=" + page_size;
-        }else{prev = null;}
-        let data = {
-            total: result.totalDocs,
-            next: next,
-            prev: prev,
-            result: result.docs
-        }
-        return res.send({
-            "status": 1,
-            "data": {...data}
+    if(!req.user.permissions.includes('123')){
+        res.status(400).send({
+            "status": 0,
+            "message": "This user does not have perrmission to view orders."
         });
-    });
+    }else{
+        let errHappen = false;
+        let err;
+        let page;
+        if(req.query.page){page = req.query.page;}else{page = 1;}
+        let page_size;
+        if(req.query.page_size){page_size = req.query.page_size;}else{page_size = 10;}
+        const options = {
+            page: page,
+            limit: page_size,
+            sort: { createdAt: -1 },
+            collation: {
+            locale: 'en'
+            }
+        };
+        let filters;
+        if(req.user.type == 'admin'){
+            filters = {parent: req.user._id};
+        }else if(req.user.type == 'staff'){
+            filters = {parent: req.user.parent};
+        }
+        if(req.query._id){filters._id = req.query._id}
+        if(req.query.canceled){filters.canceled = req.query.canceled}
+        if(req.query.branch_id){filters.branch_id = req.query.branch_id}
+        if(req.query.createdDateFrom){
+            if(new Date(req.query.createdDateFrom) == "Invalid Date"){
+                errHappen = true;
+                err = {
+                    "status": 0,
+                    "message": "Invalid createdDateFrom."
+                }
+            }
+            filters.createdDate = {$gte: new Date(req.query.createdDateFrom)}
+        }
+        if(req.query.createdDateTo){
+            if(new Date(req.query.createdDateTo) == "Invalid Date"){
+                errHappen = true;
+                err = {
+                    "status": 0,
+                    "message": "Invalid createdDateTo."
+                }
+            }
+            if(req.query.createdDateFrom){
+                filters.createdDate = {$gte: new Date(req.query.createdDateFrom), $lte: new Date(req.query.createdDateTo)}
+            }else{
+                filters.createdDate = {$lte: new Date(req.query.createdDateTo)}
+            }
+        }
+        if(errHappen){
+            res.status(400).send(err);
+        }else{
+            Order.paginate(filters, options, function(err, result) {
+                let next;
+                if(result.hasNextPage){
+                    next = "https://" + req.headers.host + "/api/order/list?page=" + result.nextPage + "&page_size=" + page_size;
+                }else{next = null;}
+                let prev;
+                if(result.hasPrevPage){
+                    prev = "https://" + req.headers.host + "/api/order/list?page=" + result.prevPage + "&page_size=" + page_size;
+                }else{prev = null;}
+                let data = {
+                    total: result.totalDocs,
+                    next: next,
+                    prev: prev,
+                    result: result.docs
+                }
+                return res.send({
+                    "status": 1,
+                    "data": {...data}
+                });
+            });
+        }
+    }
 });
 
 module.exports = router;
