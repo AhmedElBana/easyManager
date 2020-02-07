625c0005ab681d437b433c0c00810dfb3aa71c34
Ahmed Elbanna <ahmedelpna@gmail.com>
2020-02-07 23:11:10 +0200

    add product list endpoint

--- routes/product.js
+++ routes/product.js
@@ -241,4 +241,61 @@ router.get('/search', authenticate, function(req, res, next){
         }
     }
 });
+router.get('/list', authenticate, function(req, res, next) {
+    if(!req.user.permissions.includes('115')){
+        res.status(400).send({
+            "status": 0,
+            "message": "This user does not have perrmission to view products."
+        });
+    }else{
+        let errHappen = false;
+        let err;
+        let page;
+        if(req.query.page){page = req.query.page;}else{page = 1;}
+        let page_size;
+        if(req.query.page_size){page_size = req.query.page_size;}else{page_size = 10;}
+        const options = {
+            page: page,
+            limit: page_size,
+            sort: { createdAt: -1 },
+            collation: {
+            locale: 'en'
+            }
+        };
+        let filters;
+        if(req.user.type == 'admin'){
+            filters = {parent: req.user._id};
+        }else if(req.user.type == 'staff'){
+            filters = {parent: req.user.parent};
+        }
+        if(req.query._id){filters._id = req.query._id}
+        if(req.query.branch_id){
+            filters["map." + req.query.branch_id] = { $exists: true };
+        }
+        if(errHappen){
+            res.status(400).send(err);
+        }else{
+            Product.paginate(filters, options, function(err, result) {
+                let next;
+                if(result.hasNextPage){
+                    next = "https://" + req.headers.host + "/api/product/list?page=" + result.nextPage + "&page_size=" + page_size;
+                }else{next = null;}
+                let prev;
+                if(result.hasPrevPage){
+                    prev = "https://" + req.headers.host + "/api/product/list?page=" + result.prevPage + "&page_size=" + page_size;
+                }else{prev = null;}
+                let data = {
+                    total: result.totalDocs,
+                    next: next,
+                    prev: prev,
+                    result: result.docs
+                }
+                return res.send({
+                    "status": 1,
+                    "data": {...data}
+                });
+            });
+        }
+    }
+});
 module.exports = router;
