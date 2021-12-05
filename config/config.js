bd65bb4f0c399fc3efb16c6799c3c9f1a7c06b7c
ahmedelbanna <ahmedelbanna@weaccept.co>
2021-12-05 03:56:19 +0200

    move to new db

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-let env = process.env.NODE_ENV || 'development';
+let env = process.env.NODE_ENV || 'test';
 
 if(env === 'development' || env === 'test'){
 	let config = require('./config.json');
--- db/mongoose.js
+++ db/mongoose.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 mongoose.set('useCreateIndex', true);
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/easyManager', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/easyManager', { useNewUrlParser: true, useUnifiedTopology: true });
 
 module.exports = {mongoose}
